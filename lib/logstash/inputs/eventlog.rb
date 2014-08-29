75b522110acbf201243bbc6746261e8d2d8b5ca8
someperson <stownsend42@sbcglobal.net>
2014-08-29 13:38:42 -0500

    Fix for eventlog plugin not shutting down
    https://github.com/elasticsearch/logstash/issues/1672

--- lib/logstash/inputs/eventlog.rb
+++ lib/logstash/inputs/eventlog.rb
@@ -50,8 +50,12 @@ class LogStash::Inputs::EventLog < LogStash::Inputs::Base
 
       events = @wmi.ExecNotificationQuery(wmi_query)
 
-      while
-        notification = events.NextEvent
+      while true
+        begin
+          notification = events.NextEvent(1000) #timeout is 1000 ms
+        rescue
+          next
+        end
         event = notification.TargetInstance
 
         timestamp = to_timestamp(event.TimeGenerated)
@@ -88,6 +92,8 @@ class LogStash::Inputs::EventLog < LogStash::Inputs::Base
         queue << e
 
       end # while
+
+    rescue LogStash::ShutdownSignal
 
     rescue Exception => ex
       @logger.error("Windows Event Log error: #{ex}\n#{ex.backtrace}")
