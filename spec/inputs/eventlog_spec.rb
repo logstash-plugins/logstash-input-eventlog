48e81fcf9e742644aaf4ac119a09e43d162023ec
Joao Duarte <jsvduarte@gmail.com>
2015-09-17 17:27:40 +0100

    use plugin.stop to return from run
    Fixes #19

--- spec/inputs/eventlog_spec.rb
+++ spec/inputs/eventlog_spec.rb
@@ -2,4 +2,7 @@ require "logstash/devutils/rspec/spec_helper"
 require 'logstash/inputs/eventlog'
 
 describe LogStash::Inputs::EventLog do
+  it_behaves_like "an interruptible input plugin" do
+    let(:config) { { "logfile" => "Application" } }
+  end
 end
