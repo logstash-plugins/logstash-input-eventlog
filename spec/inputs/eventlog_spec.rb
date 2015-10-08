db60473ccbbebe6be183f48b6c7ca5a118a4da74
Joao Duarte <jsvduarte@gmail.com>
2015-10-08 14:58:34 +0100

    only run specs on windows

--- spec/inputs/eventlog_spec.rb
+++ spec/inputs/eventlog_spec.rb
@@ -1,7 +1,7 @@
 require "logstash/devutils/rspec/spec_helper"
 require 'logstash/inputs/eventlog'
 
-describe LogStash::Inputs::EventLog do
+describe LogStash::Inputs::EventLog, :windows => true do
   it_behaves_like "an interruptible input plugin" do
     let(:config) { { "logfile" => "Application" } }
   end
